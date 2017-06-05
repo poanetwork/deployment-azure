525317f67e9390897f61cd06988c5646fd94918c
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2017-06-05 23:05:00 +0300

    Exit on first error from script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+set -e
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo apt-key fingerprint 0EBFCD88

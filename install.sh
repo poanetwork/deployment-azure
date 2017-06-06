6401f3429624a65f72ca2a931c9d7336c5ba035d
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2017-06-06 13:42:15 +0300

    Replace timesyncd with ntpd. Close #2

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 set -e
+# install ntpd, replace timesyncd
+sudo timedatectl set-ntp no
+sudo apt-get -y install ntp
+
+# download and install docker
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo apt-key fingerprint 0EBFCD88
@@ -7,11 +12,24 @@ sudo apt-get update
 #sudo apt-get -y install docker-ce
 #apt-cache madison docker-ce
 sudo apt-get -y install docker-ce=17.03.1~ce-0~ubuntu-xenial
+
+# pull image, configs and setup parity
 sudo docker pull ethcore/parity:stable
-sudo docker
 curl -O 'https://raw.githubusercontent.com/oraclesorg/test-templates/master/demo-spec.json'
 curl -O 'https://raw.githubusercontent.com/oraclesorg/test-templates/master/node.pwds'
 curl -O 'https://raw.githubusercontent.com/oraclesorg/test-templates/master/node-to-enode.toml'
 sed -i 's/@172.16./@/g' node-to-enode.toml
 mkdir parity-data
-sudo docker run --name eth-parity -d -p 30300:30300 -p 8080:8080 -p 8180:8180 -p 8540:8540 -v $(pwd)/node.pwds:/build/node.pwds -v $(pwd)/parity-data:/tmp/parity -v $(pwd)/demo-spec.json:/build/demo-spec.json -v $(pwd)/node-to-enode.toml:/build/node-to-enode.toml ethcore/parity:stable --config node-to-enode.toml
+
+# start parity in docker
+sudo docker run -d \
+    --name eth-parity \
+    -p 30300:30300 \
+    -p 8080:8080 \
+    -p 8180:8180 \
+    -p 8540:8540 \
+    -v "$(pwd)/node.pwds:/build/node.pwds" \
+    -v "$(pwd)/parity-data:/tmp/parity" \
+    -v "$(pwd)/demo-spec.json:/build/demo-spec.json" \
+    -v "$(pwd)/node-to-enode.toml:/build/node-to-enode.toml" \
+    ethcore/parity:stable --config "node-to-enode.toml"

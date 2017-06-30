465b36786bdc777d972f14ae59651643f31b1b95
phahulin <p.hahulin@hintsolutions.ru>
2017-06-30 14:43:38 +0300

    Switch to use our docker-run image

--- TestTestNet/mining-node/install.sh
+++ TestTestNet/mining-node/install.sh
@@ -225,12 +225,12 @@ EOF
 
 setup_autoupdate() {
     echo "=====> setup_autoupdate"
-    docker pull itech/docker-run
+    docker pull oraclesorg/docker-run
     cat > /etc/cron.daily/docker-autoupdate << EOF
 #!/bin/sh
 echo "Starting: $(date)" >> /home/${ADMIN_USERNAME}/docker-autoupdate.out
 echo "Starting: $(date)" >> /home/${ADMIN_USERNAME}/docker-autoupdate.err
-sudo docker run --rm -v /var/run/docker.sock:/tmp/docker.sock itech/docker-run update >> /home/${ADMIN_USERNAME}/docker-autoupdate.out 2>> /home/${ADMIN_USERNAME}/docker-autoupdate.err
+sudo docker run --rm -v /var/run/docker.sock:/tmp/docker.sock oraclesorg/docker-run update >> /home/${ADMIN_USERNAME}/docker-autoupdate.out 2>> /home/${ADMIN_USERNAME}/docker-autoupdate.err
 echo "" >> /home/${ADMIN_USERNAME}/docker-autoupdate.out
 echo "" >> /home/${ADMIN_USERNAME}/docker-autoupdate.err
 EOF

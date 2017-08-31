e31ad053f562a4111e5064ac08fa97934815e12f
phahulin <p.hahulin@hintsolutions.ru>
2017-08-31 16:26:46 +0300

    Start service via systemd and configure logrotate

--- TestTestNet/bootnode/install.sh
+++ TestTestNet/bootnode/install.sh
@@ -23,6 +23,7 @@ start_logentries() {
     touch /home/${ADMIN_USERNAME}/logs/explorer.out
     touch /home/${ADMIN_USERNAME}/logs/parity.err
     touch /home/${ADMIN_USERNAME}/logs/parity.out
+    touch /home/${ADMIN_USERNAME}/logs/parity.log
 
     sudo bash -c "cat >> /etc/le/config << EOF
 [install_err]
@@ -63,6 +64,10 @@ destination = AlphaTestTestNet/${EXT_IP}
 
 [parity_out]
 path = /home/${ADMIN_USERNAME}/logs/parity.out
+destination = AlphaTestTestNet/${EXT_IP}
+
+[parity_log]
+path = /home/${ADMIN_USERNAME}/logs/parity.log
 destination = AlphaTestTestNet/${EXT_IP}
 
 EOF"
@@ -135,6 +140,7 @@ prepare_homedir() {
     cd "/home/${ADMIN_USERNAME}"
     echo "Now changed directory to: $(pwd)"
     mkdir -p logs
+    mkdir -p logs/old
     echo "<===== prepare_homedir"
 }
 
@@ -210,6 +216,11 @@ pull_image_and_configs() {
     curl -s -o "${GENESIS_JSON}" "${GENESIS_REPO_LOC}"
     curl -s -O "${INSTALL_CONFIG_REPO}/${NODE_TOML}"
     sed -i "/\[network\]/a nat=\"extip:${EXT_IP}\"" ${NODE_TOML}
+    cat >> ${NODE_TOML} <<EOF
+[misc]
+logging="engine=trace,network=trace,discovery=trace"
+log_file = "/home/${ADMIN_USERNAME}/logs/parity.log"
+EOF
     echo "${OWNER_KEYPASS}" > "${NODE_PWD}"
     mkdir -p parity/keys/OraclesPoA
     echo ${OWNER_KEYFILE} | base64 -d > parity/keys/OraclesPoA/owner.key
@@ -236,6 +247,28 @@ install_nodejs() {
     # add symlink if it doesn't exist
     [[ ! -f /usr/bin/node ]] && sudo ln -s /usr/bin/nodejs /usr/bin/node
     echo "<===== install_nodejs"
+}
+
+start_pm2_via_systemd() {
+    echo "=====> start_pm2_via_systemd"
+        sudo bash -c "cat > /etc/systemd/system/oracles-pm2.service <<EOF
+[Unit]
+Description=oracles pm2 service
+After=network.target
+[Service]
+Type=oneshot
+RemainAfterExit=true
+User=${ADMIN_USERNAME}
+Group=${ADMIN_USERNAME}
+Environment=MYVAR=myval
+WorkingDirectory=/home/${ADMIN_USERNAME}
+ExecStart=/usr/bin/pm2 ping
+[Install]
+WantedBy=multi-user.target
+EOF"
+    sudo systemctl enable oracles-pm2
+    sudo systemctl start oracles-pm2
+    echo "<===== start_pm2_via_systemd"
 }
 
 install_dashboard() {
@@ -277,6 +310,36 @@ EOF
     chmod +x dashboard.start
     ./dashboard.start
     echo "<====== install_dashboard"
+}
+
+install_dashboard_via_systemd() {
+    echo "=====> install_dashboard_via_systemd"
+    git clone https://github.com/oraclesorg/eth-netstats
+    cd eth-netstats
+    npm install
+    sudo npm install -g grunt-cli
+    sudo npm install pm2 -g
+    grunt
+    echo "[\"${NETSTATS_SECRET}\"]" > ws_secret.json
+    cd ..
+
+    sudo bash -c "cat > /etc/systemd/system/oracles-dashboard.service <<EOF
+[Unit]
+Description=oracles dashboard service
+After=network.target
+[Service]
+User=${ADMIN_USERNAME}
+Group=${ADMIN_USERNAME}
+Environment=MYVAR=myval
+WorkingDirectory=/home/${ADMIN_USERNAME}/eth-netstats
+Restart=always
+ExecStart=/usr/bin/npm start
+[Install]
+WantedBy=multi-user.target
+EOF"
+    sudo systemctl enable oracles-dashboard
+    sudo systemctl start oracles-dashboard
+    echo "<====== install_dashboard_via_systemd"
 }
 
 # based on https://get.parity.io
@@ -325,6 +388,63 @@ EOF
     chmod +x netstats.start
     ./netstats.start
     echo "<===== install_netstats"
+}
+
+install_netstats_via_systemd() {
+    echo "=====> install_netstats_via_systemd"
+    git clone https://github.com/oraclesorg/eth-net-intelligence-api
+    cd eth-net-intelligence-api
+    #sed -i '/"web3"/c "web3": "0.19.x",' package.json
+    npm install
+    sudo npm install pm2 -g
+
+    cat > app.json << EOL
+[
+    {
+        "name"                 : "netstats_daemon",
+        "script"               : "app.js",
+        "log_date_format"      : "YYYY-MM-DD HH:mm:SS Z",
+        "error_file"           : "/home/${ADMIN_USERNAME}/logs/netstats_daemon.err",
+        "out_file"             : "/home/${ADMIN_USERNAME}/logs/netstats_daemon.out",
+        "merge_logs"           : false,
+        "watch"                : false,
+        "max_restarts"         : 100,
+        "exec_interpreter"     : "node",
+        "exec_mode"            : "fork_mode",
+        "env":
+        {
+            "NODE_ENV"         : "production",
+            "RPC_HOST"         : "localhost",
+            "RPC_PORT"         : "8545",
+            "LISTENING_PORT"   : "30300",
+            "INSTANCE_NAME"    : "${NODE_FULLNAME}",
+            "CONTACT_DETAILS"  : "${NODE_ADMIN_EMAIL}",
+            "WS_SERVER"        : "http://localhost:3000",
+            "WS_SECRET"        : "${NETSTATS_SECRET}",
+            "VERBOSITY"        : 2
+        }
+    }
+]
+EOL
+    cd ..
+    sudo bash -c "cat > /etc/systemd/system/oracles-netstats.service <<EOF
+[Unit]
+Description=oracles netstats service
+After=oracles-pm2.service
+[Service]
+Type=oneshot
+RemainAfterExit=true
+User=${ADMIN_USERNAME}
+Group=${ADMIN_USERNAME}
+Environment=MYVAR=myval
+WorkingDirectory=/home/${ADMIN_USERNAME}/eth-net-intelligence-api
+ExecStart=/usr/bin/pm2 startOrRestart app.json
+[Install]
+WantedBy=multi-user.target
+EOF"
+    sudo systemctl enable oracles-netstats
+    sudo systemctl start oracles-netstats
+    echo "<===== install_netstats_via_systemd"
 }
 
 install_chain_explorer() {
@@ -387,6 +507,74 @@ EOF
     echo "<===== install_chain_explorer"
 }
 
+install_chain_explorer_via_systemd() {
+    echo "=====> install_chain_explorer_via_systemd"
+    git clone https://github.com/oraclesorg/chain-explorer
+    git clone https://github.com/ethereum/solc-bin chain-explorer/utils/solc-bin
+    cd chain-explorer
+    npm install
+    sudo npm install pm2 -g
+    cat > config.js <<EOF
+var web3 = require('web3');
+var net = require('net');
+var config = function () {
+    this.logFormat = "combined";
+    this.ipcPath = "/home/${ADMIN_USERNAME}/parity/jsonrpc.ipc";
+    this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
+    this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
+    this.names = {
+        "0xdd0bb0e2a1594240fed0c2f2c17c1e9ab4f87126": "Bootnode",
+    };
+}
+module.exports = config;
+EOF
+#    sudo apt-get install -y dtach
+#    cat > explorer.start <<EOF
+#dtach -n explorer bash -c "cd chain-explorer; PORT=4000 npm start > ../logs/explorer.out 2> ../logs/explorer.err"
+#EOF
+
+    cat > app.json << EOF
+[
+    {
+        "name"                 : "explorer",
+        "script"               : "./bin/www",
+        "log_date_format"      : "YYYY-MM-DD HH:mm:SS Z",
+        "error_file"           : "/home/${ADMIN_USERNAME}/logs/explorer.err",
+        "out_file"             : "/home/${ADMIN_USERNAME}/logs/explorer.out",
+        "merge_logs"           : false,
+        "watch"                : false,
+        "max_restarts"         : 100,
+        "exec_interpreter"     : "node",
+        "exec_mode"            : "fork_mode",
+        "env":
+        {
+            "NODE_ENV"         : "production",
+            "PORT"             : 4000,
+        }
+    }
+]
+EOF
+    cd ..
+    sudo bash -c "cat > /etc/systemd/system/oracles-chain-explorer.service <<EOF
+[Unit]
+Description=oracles chain explorer service
+After=oracles-pm2.service
+[Service]
+Type=oneshot
+RemainAfterExit=true
+User=${ADMIN_USERNAME}
+Group=${ADMIN_USERNAME}
+Environment=MYVAR=myval
+WorkingDirectory=/home/${ADMIN_USERNAME}/chain-explorer
+ExecStart=/usr/bin/pm2 startOrRestart app.json
+[Install]
+WantedBy=multi-user.target
+EOF"
+    sudo systemctl enable oracles-chain-explorer
+    sudo systemctl start oracles-chain-explorer
+    echo "<===== install_chain_explorer_via_systemd"
+}
+
 start_docker() {
     echo "=====> start_docker"
     cat > docker.start << EOF
@@ -422,6 +610,29 @@ EOF
     chmod +x parity.start
     ./parity.start
     echo "<===== use_deb"
+}
+
+use_deb_via_systemd() {
+    echo "=====> use_deb_via_systemd"
+    curl -LO 'http://parity-downloads-mirror.parity.io/v1.7.0/x86_64-unknown-linux-gnu/parity_1.7.0_amd64.deb'
+    sudo dpkg -i parity_1.7.0_amd64.deb
+
+    sudo bash -c "cat > /etc/systemd/system/oracles-parity.service <<EOF
+[Unit]
+Description=oracles parity service
+After=network.target
+[Service]
+User=${ADMIN_USERNAME}
+Group=${ADMIN_USERNAME}
+WorkingDirectory=/home/${ADMIN_USERNAME}
+ExecStart=/usr/bin/parity --config=node.toml --ui-no-validation
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF"
+    sudo systemctl enable oracles-parity
+    sudo systemctl start oracles-parity
+    echo "<===== use_deb_via_systemd"
 }
 
 use_bin() {
@@ -478,6 +689,33 @@ EOF"
     echo "<===== setup_autoupdate"
 }
 
+configure_logrotate() {
+    echo "=====> configure_logrotate"
+    
+    sudo bash -c "cat > /etc/logrotate.d/oracles.conf << EOF
+/home/${ADMIN_USERNAME}/logs/*.log {
+    rotate 10
+    size 200M
+    missingok
+    compress
+    copytruncate
+    dateext
+    dateformat %Y-%m-%d-%s
+    olddir old
+}
+/home/${ADMIN_USERNAME}/.pm2/pm2.log {
+    su ${ADMIN_USERNAME} ${ADMIN_USERNAME}
+    rotate 10
+    size 200M
+    missingok
+    compress
+    copytruncate
+    dateext
+    dateformat %Y-%m-%d-%s
+}"
+    echo "<===== configure_logrotate"
+}
+
 download_initial_keys_script() {
     echo "=====> download_initial_keys_script"
     git clone https://github.com/oraclesorg/oracles-initial-keys
@@ -503,16 +741,22 @@ main () {
     clone_dapps
 
     #start_docker
-    use_deb
+    #use_deb
+    use_deb_via_systemd
     #use_bin
     #compile_source
 
     #setup_autoupdate
 
-    install_dashboard
-    install_netstats
-    install_chain_explorer
-    
+    #install_dashboard
+    install_dashboard_via_systemd
+    #install_netstats
+    install_netstats_via_systemd
+    #install_chain_explorer
+    install_chain_explorer_via_systemd
+
+    configure_logrotate
+
     download_initial_keys_script
 }
 

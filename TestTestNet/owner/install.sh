25faff0e0c371c49c4ca90a93afe5fca69860657
phahulin <p.hahulin@hintsolutions.ru>
2017-11-17 12:49:12 +0300

    Update install.sh

--- TestTestNet/owner/install.sh
+++ TestTestNet/owner/install.sh
@@ -14,18 +14,15 @@ lsb_release -a
 echo "===== memory usage info:"
 free -m
 
-echo "===== downloading common.vars"
-curl -sLO "https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/common.vars"
-source common.vars
-
 EXT_IP="$(curl ifconfig.co)"
 echo "===== external ip: ${EXT_IP}"
 
-echo "===== environmental variables:"
-printenv
-
 INSTALL_CONFIG_REPO="https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/owner"
 echo "===== repo base path: ${INSTALL_CONFIG_REPO}"
+
+echo "===== downloading common.vars"
+curl -sLO "https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/common.vars"
+source common.vars
 
 # this should be provided through env by azure template
 NETSTATS_SERVER="${NETSTATS_SERVER}"
@@ -37,6 +34,9 @@ NODE_ADMIN_EMAIL="${NODE_ADMIN_EMAIL:-somebody@somehere}"
 ADMIN_USERNAME="${ADMIN_USERNAME}"
 
 export HOME="${HOME:-/home/${ADMIN_USERNAME}}"
+
+echo "===== environmental variables:"
+printenv
 
 echo "===== downloading common.funcs"
 curl -sLO "https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/common.funcs"

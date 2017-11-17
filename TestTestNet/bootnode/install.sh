42b3df3f7dc4028b483667e6c35140fcfdbde6aa
phahulin <p.hahulin@hintsolutions.ru>
2017-11-17 12:47:40 +0300

    Update install.sh

--- TestTestNet/bootnode/install.sh
+++ TestTestNet/bootnode/install.sh
@@ -21,9 +21,6 @@ source common.vars
 EXT_IP="$(curl ifconfig.co)"
 echo "===== external ip: ${EXT_IP}"
 
-echo "===== environmental variables:"
-printenv
-
 NGINX_FILE_LOC="https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/bootnode/nginx.default.site"
 
 INSTALL_CONFIG_REPO="https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/bootnode"
@@ -37,6 +34,9 @@ NODE_ADMIN_EMAIL="${NODE_ADMIN_EMAIL:-somebody@somehere}"
 ADMIN_USERNAME="${ADMIN_USERNAME}"
 
 export HOME="${HOME:-/home/${ADMIN_USERNAME}}"
+
+echo "===== environmental variables:"
+printenv
 
 echo "===== downloading common.funcs"
 curl -sLO "https://raw.githubusercontent.com/oraclesorg/test-templates/${TEMPLATES_BRANCH}/TestTestNet/common.funcs"
